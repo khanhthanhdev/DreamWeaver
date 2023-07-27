e996c62e8f6deb472deab3fc433202199791e2b2
khanhthanhdev <thanhkt27507@gmail.com>
2023-07-27 09:09:14 +0700

    mobile sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from 'lucide-react';
 import { Montserrat } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -8,7 +9,52 @@ import React from 'react'
 
 const montserrat = Montserrat({weight: "600", subsets: ["latin"]})
 
+const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-500"
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-500"
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-pink-500"
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-500"
+    },
+    {
+        label: "Music Generation",
+        icon: Music,
+        href: "/music",
+        color: "text-emerald-500"
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        href: "/code",
+        color: "text-green-700"
+    },
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/settings",
+    },
+]
+
 const Sidebar = () => {
+
   return (
     <div className='space-y-4 py-4 flex flex-col h-full
         bg-[#111827] text-white
@@ -26,6 +72,23 @@ const Sidebar = () => {
                     DreamWeaver
                 </h1>
             </Link>
+            <div className='space-y-1'>
+                {routes.map((route) => (
+                    <Link
+                        href={route.href}
+                        key={route.href}
+                        className="text-sm group flex p-3 w-full
+                            justify-start font-medium cursor-pointer
+                            hover:text-white hover:bg-white/10 rounded-lg transition
+                        "
+                    >
+                        <div className='flex items-center flex-1'>
+                            <route.icon className={cn('w-5 h-5 mr-3', route.color)} />
+                            {route.label}
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     </div>
   )
